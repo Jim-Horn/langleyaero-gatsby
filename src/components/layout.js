ddec58720213f334c3dcdb726cf51aa8f310c824
Jim Horn <jim@jdhorn.com>
2023-01-27 07:47:00 -0800

    Include AUI in template

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,6 +7,7 @@
 
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
+import { GlobalStyles } from "@soluto-private/mx-asurion-ui-react";
 
 import Header from "./header"
 import "./layout.css"
@@ -24,6 +25,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
+      <GlobalStyles/>
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
       <div
         style={{

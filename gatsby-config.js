1659a5a4af50605ff86e3a7e89ee810143c66004
Jim Horn <jim@jdhorn.com>
2023-01-31 06:37:44 -0800

    Load Contentful conditionally depending on presence of `CONTENTFUL_SPACE_ID`

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,42 @@
 require("dotenv").config();
+
+const plugins = [
+    `gatsby-plugin-image`,
+    {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+            name: `images`,
+            path: `${__dirname}/src/images`,
+        },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-styled-components`,
+    {
+        resolve: `gatsby-plugin-manifest`,
+        options: {
+            name: `gatsby-starter-default`,
+            short_name: `starter`,
+            start_url: `/`,
+            background_color: `#663399`,
+            // This will impact how browsers show your PWA/website
+            // https://css-tricks.com/meta-theme-color-and-trickery/
+            // theme_color: `#663399`,
+            display: `minimal-ui`,
+            icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        },
+    },
+];
+
+!!process.env.CONTENTFUL_SPACE_ID &&
+    plugins.push({
+        resolve: `gatsby-source-contentful`,
+        options: {
+            spaceId: process.env.CONTENTFUL_SPACE_ID,
+            accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        },
+    });
+
 /**
  * Configure your Gatsby site with this file.
  *
@@ -15,38 +53,5 @@ module.exports = {
         author: `@gatsbyjs`,
         siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
     },
-    plugins: [
-        {
-            resolve: `gatsby-source-contentful`,
-            options: {
-                spaceId: process.env.CONTENTFUL_SPACE_ID,
-                accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-            },
-        },
-        `gatsby-plugin-image`,
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `images`,
-                path: `${__dirname}/src/images`,
-            },
-        },
-        `gatsby-transformer-sharp`,
-        `gatsby-plugin-sharp`,
-        `gatsby-plugin-styled-components`,
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: `gatsby-starter-default`,
-                short_name: `starter`,
-                start_url: `/`,
-                background_color: `#663399`,
-                // This will impact how browsers show your PWA/website
-                // https://css-tricks.com/meta-theme-color-and-trickery/
-                // theme_color: `#663399`,
-                display: `minimal-ui`,
-                icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-            },
-        },
-    ],
+    plugins,
 };
